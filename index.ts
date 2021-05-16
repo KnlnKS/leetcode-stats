3b001c48c85125a8c5f34db2f358d5e5154ebe85
KnlnKS <soulkks@hotmail.ca>
2021-05-16 00:40:10 -0400

    🍱 Create stats.svg

--- index.ts
+++ index.ts
@@ -2,6 +2,11 @@ import { request, gql } from "graphql-request";
 
 const query = gql`
   query getUserProfile($username: String!) {
+    allQuestionsCount {
+      difficulty
+      count
+      __typename
+    }
     matchedUser(username: $username) {
       username
       socialAccounts
@@ -13,26 +18,33 @@ const query = gql`
         __typename
       }
       profile {
-        realName
-        websites
-        countryName
-        skillTags
-        company
-        school
         starRating
-        aboutMe
-        userAvatar
-        reputation
         ranking
         __typename
       }
+      submitStats {
+        acSubmissionNum {
+          difficulty
+          count
+          submissions
+          __typename
+        }
+        totalSubmissionNum {
+          difficulty
+          count
+          submissions
+          __typename
+        }
+        __typename
+      }
+
       __typename
     }
   }
 `;
 
-const variables = {"username": "knlnks"}
+const variables = { username: "knlnks" };
 
 request("https://leetcode.com/graphql", query, variables).then((data) =>
-  console.log(data)
+  console.log(data?.matchedUser?.submitStats?.acSubmissionNum)
 );
