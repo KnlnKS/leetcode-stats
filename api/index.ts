fc0bb16463eacd9bd85ca8df4ec1c347bf937ffd
KnlnKS <soulkks@hotmail.ca>
2021-07-05 23:48:49 -0400

    🔧 Update Config

--- api/index.ts
+++ api/index.ts
@@ -3,8 +3,9 @@ import { generateStatSVG, getSubmissionStats } from "./svg";
 
 const app = express();
 
+app.get("/", (_, res) => res.send("<em>Nothing here...</em>"));
+
 app.get("/api", async (req, res) => {
-  /*
   const { username } = req?.query;
 
   if (username) {
@@ -15,11 +16,13 @@ app.get("/api", async (req, res) => {
       },
     } = await getSubmissionStats(username);
 
-
     res.setHeader("Content-Type", "image/svg+xml");
-    res.setHeader('Cache-Control', 's-max-age=60, stale-while-revalidate');
-    res.status(200).end(generateStatSVG(allQuestionsCount, acSubmissionNum));
-  } else res.status(400).end("Missing Parameter");
-  */
- res.end("yo")
+    res.setHeader("Cache-Control", "s-max-age=60, stale-while-revalidate");
+    res.status(200).send(generateStatSVG(allQuestionsCount, acSubmissionNum));
+  } else res.status(400).send("Missing Parameter");
 });
+
+const port = process.env.PORT || 2001;
+app.listen(port, () =>
+  console.log(`Server running on ${port}, http://localhost:${port}`)
+);
