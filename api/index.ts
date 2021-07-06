0af5d13d2ad17083e7486b2b1dda367c3de7f640
KnlnKS <soulkks@hotmail.ca>
2021-07-05 23:42:19 -0400

    ⚗️ Test

--- api/index.ts
+++ api/index.ts
@@ -4,6 +4,7 @@ import { generateStatSVG, getSubmissionStats } from "./svg";
 const app = express();
 
 app.get("/api", async (req, res) => {
+  /*
   const { username } = req?.query;
 
   if (username) {
@@ -19,4 +20,6 @@ app.get("/api", async (req, res) => {
     res.setHeader('Cache-Control', 's-max-age=60, stale-while-revalidate');
     res.status(200).end(generateStatSVG(allQuestionsCount, acSubmissionNum));
   } else res.status(400).end("Missing Parameter");
+  */
+ res.end("yo")
 });
