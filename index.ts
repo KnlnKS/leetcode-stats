e7cba335c92c58752cf8db54a8f23286d0e34cc7
KnlnKS <soulkks@hotmail.ca>
2021-07-06 00:44:44 -0400

    🥅 Catch Errors

--- index.ts
+++ index.ts
@@ -5,20 +5,27 @@ const app = express();
 
 app.get("/", (_, res) => res.send("<em>Nothing here...</em>"));
 
-app.get("/api", async (req, res) => {
+app.get("/api", async (req, res, next) => {
   const { username } = req?.query;
 
   if (username) {
-    const {
-      allQuestionsCount,
-      matchedUser: {
-        submitStats: { acSubmissionNum },
-      },
-    } = await getSubmissionStats(username);
+    let data;
 
-    res.setHeader("Content-Type", "image/svg+xml");
-    res.setHeader("Cache-Control", "s-max-age=60, stale-while-revalidate");
-    res.status(200).send(generateStatSVG(allQuestionsCount, acSubmissionNum));
+    try {
+      data = await getSubmissionStats(username);
+      const {
+        allQuestionsCount,
+        matchedUser: {
+          submitStats: { acSubmissionNum },
+        },
+      } = data;
+
+      res.setHeader("Content-Type", "image/svg+xml");
+      res.setHeader("Cache-Control", "s-max-age=60, stale-while-revalidate");
+      res.status(200).send(generateStatSVG(allQuestionsCount, acSubmissionNum));
+    } catch (err) {
+      res.status(400).send("Username does not exist");
+    }
   } else res.status(400).send("Missing Parameter");
 });
 
