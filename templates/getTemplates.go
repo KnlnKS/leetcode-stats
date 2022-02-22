cb770a30111a339db5753fc2a80a43a87b0f2ed9
KnlnKS <soulkks@hotmail.ca>
2022-02-22 18:36:48 -0500

    ✨ Add Template

---
+++ templates/getTemplates.go
@@ -0,0 +1,20 @@
+package templates
+
+import (
+	"html/template"
+	"path/filepath"
+	"runtime"
+)
+
+var (
+	_, b, _, _ = runtime.Caller(0)
+	basepath   = filepath.Dir(b)
+)
+
+func GetSubmissionStatsTemplate() *template.Template {
+	tmpl, err := template.ParseFiles(basepath + "/submissionData.svg")
+	if err != nil {
+		panic(err)
+	}
+	return tmpl
+}
