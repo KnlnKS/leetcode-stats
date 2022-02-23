a6e25608dd17f51538339403b0f972253b62948a
KnlnKS <soulkks@hotmail.ca>
2022-02-23 14:53:51 -0500

    ⚗️ Trying Something Out

--- api/index.go
+++ api/index.go
@@ -2,9 +2,14 @@ package handler
 
 import (
 	"fmt"
-	"leetcode-stats/src"
-	"leetcode-stats/templates"
 	"net/http"
+	"path/filepath"
+	"runtime"
+)
+
+var (
+	_, b, _, _ = runtime.Caller(0)
+	basepath   = filepath.Dir(b)
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
@@ -12,26 +17,8 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "image/svg+xml")
 	w.Header().Add("Cache-Control", "s-max-age=60, stale-while-revalidate")
 	if q["username"] != nil && len(q["username"][0]) > 0 {
-		submissionData, err := src.GetSubmissionStats(q["username"][0])
-		if err != nil {
-			fmt.Fprintf(w, "Error: %v", err)
-			return
-		}
-		var theme src.Theme
-		if q["theme"] != nil && len(q["theme"][0]) > 0 {
-			theme = src.GenerateTheme(q["theme"][0])
-		} else {
-			theme = src.GenerateTheme("light")
-		}
 
-		tmpl := templates.GetSubmissionStatsTemplate()
-		tmpl.Execute(w, struct {
-			SubmissionData src.SubmissionData
-			Theme          src.Theme
-		}{
-			SubmissionData: submissionData,
-			Theme:          theme,
-		})
+		fmt.Fprintf(w, basepath)
 	} else {
 		fmt.Fprintf(w, "<svg>Something went wrong s</svg>")
 	}
