b3e8e956b6c0c1074b7c7afd401c3ac42fc2f001
KnlnKS <soulkks@hotmail.ca>
2022-02-25 14:30:51 -0500

    ⚗️ Trying Something Out

--- templates/getTemplates.go
+++ templates/getTemplates.go
@@ -1,7 +1,10 @@
 package templates
 
 import (
+	"fmt"
 	"html/template"
+	"log"
+	"os"
 	"path/filepath"
 	"runtime"
 )
@@ -11,7 +14,25 @@ var (
 	basepath   = filepath.Dir(b)
 )
 
+func visit(files *[]string) filepath.WalkFunc {
+	return func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Fatal(err)
+		}
+		*files = append(*files, path)
+		return nil
+	}
+}
+
 func GetSubmissionStatsTemplate() *template.Template {
+	var files []string
+	err := filepath.Walk(basepath, visit(&files))
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range files {
+		fmt.Println(file)
+	}
 	tmpl, err := template.ParseFiles(basepath + "/submissionData.svg")
 	if err != nil {
 		panic(err)
