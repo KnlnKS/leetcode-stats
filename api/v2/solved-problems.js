ef862349d8368afe25bd51954870b44661d0dd3d
KnlnKS <soulkks@hotmail.ca>
2022-03-14 15:18:17 -0400

    🔥 Remove Code

--- api/v2/solved-problems.js
+++ api/v2/solved-problems.js
@@ -6,14 +6,10 @@ async function handler(req, res) {
     res.status(400).send("Username is required");
     return;
   }
-  if (theme !== "light" && theme !== "dark") {
-    res.status(400).send("Theme must be either 'light' or 'dark'");
-    return;
-  }
+
   const chrome = new Chrome();
   await chrome.getPage(`https://leetcode.com/${username}`, theme);
   const image = await chrome.takeScreenshot(".min-w-max");
-  chrome.close();
 
   res.setHeader("Content-Type", "image/png");
   res.status(200).send(image);
--- api/v2/submission-calendar.js
+++ api/v2/submission-calendar.js
@@ -6,10 +6,6 @@ async function handler(req, res) {
     res.status(400).send("Username is required");
     return;
   }
-  if (theme !== "light" && theme !== "dark") {
-    res.status(400).send("Theme must be either 'light' or 'dark'");
-    return;
-  }
 
   const chrome = new Chrome();
   await chrome.getPage(`https://leetcode.com/${username}`, theme);
@@ -31,7 +27,6 @@ async function handler(req, res) {
     '//*[@id="__next"]/body/div/div/div/div[2]/div[2]/div',
     true
   );
-  chrome.close();
 
   res.setHeader("Content-Type", "image/png");
   res.status(200).send(image);
