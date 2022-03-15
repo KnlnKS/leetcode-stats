6a3067c49cda8f6aa0dcbe413a5ffc90266da403
KnlnKS <soulkks@hotmail.ca>
2022-03-15 01:04:37 -0400

    🎨 Restructure Code

--- index.js
+++ index.js
@@ -63,3 +63,28 @@ export default class Chrome {
     return await this.page.evaluate(fn);
   }
 }
+
+async function handler(event) {
+  const { username, theme = "light" } = event.queryStringParameters;
+  if (!username) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: `Error, no username provided!` }),
+    };
+  }
+
+  const chrome = new Chrome();
+  await chrome.getPage(`https://leetcode.com/${username}`, theme);
+  const image = await chrome.takeScreenshot(".min-w-max");
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-type": "image/png",
+    },
+    body: image.toString("base64"),
+    isBase64Encoded: true,
+  };
+}
+
+module.exports = { handler };
