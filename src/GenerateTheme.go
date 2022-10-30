b2bd8aae5becade167896b86600ba611c1ef234f
KnlnKS <soulkks@hotmail.ca>
2022-10-30 05:35:55 -0400

    💄 Add dark theme

--- src/GenerateTheme.go
+++ src/GenerateTheme.go
@@ -3,7 +3,9 @@ package src
 type Theme struct {
 	Background       string
 	Text             string
-	SecondaryText    string
+	Text2            string
+	Text3            string
+	Text4            string
 	EasyDifficulty   string
 	MediumDifficulty string
 	HardDifficulty   string
@@ -15,25 +17,20 @@ func GenerateTheme(theme string) Theme {
 		return Theme{
 			Background:       "#292A2B",
 			Text:             "#CFCCC7",
-			SecondaryText:    "#7A807C",
+			Text2:            "rgba(239,241,246,.75)",
+			Text3:            "rgba(239,242,246,.6)",
+			Text4:            "rgba(235,235,245,.3)",
 			EasyDifficulty:   "#43A047",
 			MediumDifficulty: "#FB8C00",
 			HardDifficulty:   "#E91E63",
-		}
-	case "midnight":
-		return Theme{
-			Background:       "#1A1B27",
-			Text:             "#70A4FC",
-			SecondaryText:    "#BE90F2",
-			EasyDifficulty:   "#38BCAD",
-			MediumDifficulty: "#38BCAD",
-			HardDifficulty:   "#38BCAD",
 		}
 	default:
 		return Theme{
 			Background:       "#FFF",
 			Text:             "#262626",
-			SecondaryText:    "#8A8A8E",
+			Text2:            "rgba(38,38,38,.75)",
+			Text3:            "rgba(60, 60, 67, .6)",
+			Text4:            "rgba(60, 60, 67, .3)",
 			EasyDifficulty:   "#43A047",
 			MediumDifficulty: "#FB8C00",
 			HardDifficulty:   "#E91E63",
